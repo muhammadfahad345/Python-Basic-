a8673da8d68daa42140196dc5058b5af85ea610c
Muhammad Fahad <91471596+muhammadfahad345@users.noreply.github.com>
2022-08-30 04:52:13 +0500

    Add files via upload

---
+++ python basic.py
@@ -0,0 +1,411 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+print("hellow world")
+# In[105]:
+
+
+a = 5;
+b = 10;
+c = a + b;
+print(c);
+
+
+# In[106]:
+
+
+x = "fahad";
+y = " manzoor";
+z = x + y;
+print(z);
+
+
+# In[107]:
+
+
+f = "4";
+g = "8";
+h = f + g;
+print(h);
+
+
+# In[108]:
+
+
+a = "5";
+b = 10;
+c = a + b;
+print(c);
+
+
+# In[ ]:
+
+
+x = 8;
+y = x + 7;
+z = y - x + 1;
+print(z);
+
+
+# In[ ]:
+
+
+a = 12 - 4 / 2 + 9 * 5;
+print(a);
+
+
+# In[ ]:
+
+
+a = ( ( 12 -4 ) / (2 + 9) * 5);
+print(a);
+
+
+# In[ ]:
+
+
+a = 5;
+b = 6;
+c = a + b
+print(c);
+
+
+# In[ ]:
+
+
+a="5";
+b="6";
+c=a+b;
+print(c);
+
+
+# In[ ]:
+
+
+x = 10;
+y = x % 3;
+print(y);
+
+
+# In[ ]:
+
+
+f = 10;
+f = 12;
+print(f);
+
+
+# In[ ]:
+
+
+g = 5;
+g = g + 2;
+print(g);
+
+
+# In[ ]:
+
+
+a = 3;
+a+=3;
+print(a);
+
+
+# In[ ]:
+
+
+x = 20
+y = 10
+a = 7
+b = 23
+
+if x + y != a + b :
+  print("your answer is correct")
+else:
+    print("wrong answer");
+
+
+# In[ ]:
+
+
+a = 3;
+b = 3
+c = a
+
+
+# # print
+
+# In[ ]:
+
+
+print (12345 , 'fadii' , 1234 , "fadii", end=' *** ' , sep=' *** ')
+print ("fadii" , end=' *** ')
+print (123)
+
+
+# # commenting
+
+# In[ ]:
+
+
+#this is a single line comment
+
+
+# In[ ]:
+
+
+"""
+thsi is mu;ltiple line comment
+agr ham na 2 sa zyda line ma apna comment likhna ha to
+3 strings use krta ha 
+"""
+
+
+# # Data Type
+
+# In[44]:
+
+
+5            # integre  > int
+10.2         #floot or double
+True         #boolean  > bool
+"Bilal"      #string  > str
+'B'          #string  > str
+
+
+# # Variable and Naming Conventions
+# 
+
+# In[45]:
+
+
+var = "Bilal"
+
+first_name = "Muhammad"
+last_name = "Bilal"
+
+firstNameAndLastName = "Muhammad"
+lastName = "Bilal"
+
+print(var)
+
+
+# # What or what cannot be a variable name?
+# 
+
+# In[ ]:
+
+
+1var (a variable name cannot start with a number)
+_var (possible)
+&var (not possible)
+
+
+# # Code Snippet and Task
+# 
+
+# In[29]:
+
+
+#This is avarage
+
+var = 95
+var = 85
+var = 73
+
+#formula; sum of total num / total number of value
+avarage = (95 + 85 + 73) / 3
+print("My avarage is:" , avarage)
+
+
+# In[47]:
+
+
+#this is pecentage
+
+#total marks 300
+
+#obtain marks
+
+var1 = 95
+var2 = 85
+var3 = 73
+print(var1 + var2 +var3)
+
+#formula obtain num /total num * 100
+
+percentage = 253 / 300 *100
+print("My percentage is:", percentage)
+
+
+# # Task 1
+
+# In[46]:
+
+
+var1 = 95
+var2 = 83
+var3 = 75
+
+total_number = 300
+obtain_mark = var1 + var2 + var3
+print(obtain_mark)
+
+percentage= obtain_mark / total_number *100
+print(percentage)
+
+
+# # Datatypes Compatibility
+# 
+
+# In[7]:
+
+
+print(3 + 4)
+print(3.0 + 4)
+print(3 * 4)
+print(3 / 4)
+
+
+# In[24]:
+
+
+print(3 + True)   #True means  1
+print(3 + False)  #False means 0
+
+
+# In[89]:
+
+
+print("fahad " * 10 )
+
+
+# # operators
+
+# In[41]:
+
+
+# / , // division operator
+
+print(5 / 3)   #simple division
+print(5 // 3)  #flood division
+
+
+# In[55]:
+
+
+# and, or , not operator
+ 
+print(True and True)
+print(True and False)
+print(True or False)
+
+
+# In[59]:
+
+
+# > , < , >= , <= greater then and less than operator 
+    
+print(5 > 4)
+print(5 < 4)
+print(5 >= 5)
+print(5 <= 4)
+    
+
+
+# In[77]:
+
+
+# = operator
+
+b = 3
+b -= 1  # 2
+b += 1  # 3
+b *= 1  # 3
+b /= 1  # 3
+print(b)
+
+
+# In[95]:
+
+
+# assigment operator
+
+c = 5
+print(c * 2)
+
+
+# In[94]:
+
+
+# exponential operator 
+#cube operatore and 4 time ya 5 time b krna ho to asa he kra ga 
+c = 5
+print(c ** 2)
+
+
+# In[79]:
+
+
+#modolo operator / modules operator
+    
+print(9 % 3)
+print(5 % 10)
+print(65 % 54)
+
+
+# In[82]:
+
+
+# equal operator
+print(5 == 5)
+print(5 == 6)
+
+
+# In[85]:
+
+
+#notequal Operator
+print(5 != 6)
+print(5 != 5)
+
+
+# # Task 2
+
+# In[99]:
+
+
+a = "Fadii "
+print(a * 10)
+
+
+# In[116]:
+
+
+x = "fadii "
+y = "fahad"
+z = x + y
+print(z)
+
+
+# In[143]:
+
+
+a = 3
+b = 5
+c = a / b
+print(c)
+
+c -= 15
+print(c)
+
+d = c // 5
+print(d)
+
+                               #task complete 
+
